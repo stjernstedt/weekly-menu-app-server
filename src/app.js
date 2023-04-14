93452df64aea3fb290aac2f9f9852560b58097a4
Mattias Stjernstedt <mstjernstedt@gmail.com>
2023-04-14 12:50:47 +0900

    handles post request for new dishes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const { start } = require('repl');
 const { connectToDb, getDb } = require('./db');
 
 const app = express();
+app.use(express.json());
 
 app.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
